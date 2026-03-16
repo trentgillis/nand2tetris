461be6976fc3fe6a15f3c0843e46269474f72614
Trentin Gillis <trentin.gillis@gmail.com>
2026-03-16 08:57:52 -0600

    project 06: complete implementation of dest

--- project06/rs/src/assembler/parser.rs
+++ project06/rs/src/assembler/parser.rs
@@ -23,6 +23,15 @@ pub fn symbol(line: &str) -> &str {
     }
 }
 
+pub fn dest(line: &str) -> Result<&str, String> {
+    let parts: Vec<&str> = line.split("=").collect();
+    if parts.len() < 2 {
+        return Err(format!("Failed to get dest at line: {line}"));
+    }
+
+    Ok(parts[0])
+}
+
 #[cfg(test)]
 mod tests {
     mod instruction_type {
@@ -46,17 +55,38 @@ mod tests {
             assert_eq!(inst_type, InstructionType::L);
         }
     }
+
     mod symbol {
         use super::super::*;
 
         #[test]
         fn test_symbol_a_instruction() {
             assert_eq!(symbol("@15"), "15");
+            assert_eq!(symbol("@myvar"), "myvar");
         }
 
         #[test]
         fn test_symbol_l_instruction() {
             assert_eq!(symbol("(LOOP)"), "LOOP");
+        }
+    }
+
+    mod dest {
+        use super::super::*;
+
+        #[test]
+        fn test_dest_with_dest() {
+            let asm = "M=D";
+            let dest = dest(asm);
+            assert!(dest.is_ok());
+            assert_eq!(dest.unwrap(), "M");
+        }
+
+        #[test]
+        fn test_dest_without_dest() {
+            let asm = "0;JEQ";
+            let dest = dest(asm);
+            assert!(dest.is_err());
         }
     }
 }
