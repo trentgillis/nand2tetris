9ee315d62354d3e23e1cadc905886ef212f1e443
Trentin Gillis <trentin.gillis@gmail.com>
2026-04-16 08:46:20 -0600

    project 08: add rs directory without accidentally overwriting the go program this time

---
+++ project08/rs/src/vm_translator.rs
@@ -0,0 +1,96 @@
+use std::{
+    error::Error,
+    fs,
+    io::{BufRead, BufReader, Read, Write},
+    path::{Path, PathBuf},
+};
+
+pub mod cli_config;
+
+mod code_writer;
+mod parser;
+
+pub fn translate(cfg: cli_config::CliConfig) -> Result<(), Box<dyn Error>> {
+    let path = Path::new(&cfg.program_path);
+    if !path.exists() {
+        return Err("The supplied program path does not exist.".into());
+    }
+
+    let output_path = if path.is_dir() {
+        path.join(
+            path.file_name()
+                .ok_or("Unable to get output_path from input")?,
+        )
+        .with_extension("asm")
+    } else {
+        path.with_extension("asm")
+    };
+    let output_file = fs::File::create(&output_path)?;
+
+    let vm_file_paths = get_vm_files(&cfg.program_path);
+    let mut vm_translator = VmTranslator::new(
+        output_file,
+        output_path
+            .file_prefix()
+            .ok_or("Unable to determine output file name")?
+            .to_str()
+            .ok_or("Output file name was not valid UTF-8")?,
+    );
+    for path in vm_file_paths? {
+        let file = fs::File::open(path)?;
+        vm_translator.translate(file)?;
+    }
+
+    Ok(())
+}
+
+fn get_vm_files(program_path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn Error>> {
+    if program_path.is_dir() {
+        Ok(fs::read_dir(program_path)?
+            .filter_map(|path| path.ok())
+            .filter(|path| path.path().extension() == Some("vm".as_ref()))
+            .map(|path| path.path())
+            .collect())
+    } else if program_path.extension() == Some("vm".as_ref()) {
+        Ok(vec![program_path.to_path_buf()])
+    } else {
+        Err("Path was not a .vm file or a directory containing at least one .vm file".into())
+    }
+}
+
+struct VmTranslator<W: Write> {
+    code_writer: code_writer::CodeWriter<W>,
+}
+
+impl<W: Write> VmTranslator<W> {
+    fn new(writer: W, program_name: impl Into<String>) -> Self {
+        let code_writer = code_writer::CodeWriter::new(writer, program_name);
+        VmTranslator { code_writer }
+    }
+
+    fn translate<R>(&mut self, r: R) -> Result<(), Box<dyn Error>>
+    where
+        R: Read,
+    {
+        let reader = BufReader::new(r);
+        for line in reader.lines() {
+            let line = line?;
+
+            if line.is_empty() || line.starts_with("//") {
+                continue;
+            }
+
+            match parser::parse(&line)? {
+                parser::Command::Push { segment, index } => {
+                    self.code_writer.write_push(segment, index)?
+                }
+                parser::Command::Pop { segment, index } => {
+                    self.code_writer.write_pop(segment, index)?
+                }
+                parser::Command::Arithmetic(cmd) => self.code_writer.write_arithmetic(cmd)?,
+            }
+        }
+
+        Ok(())
+    }
+}
---
+++ project08/rs/src/vm_translator/code_writer.rs
@@ -0,0 +1,190 @@
+use std::{collections::HashMap, error::Error, io::Write, sync::LazyLock};
+
+static SEGMENT_MAPPINGS: LazyLock<HashMap<&str, &str>> = LazyLock::new(|| {
+    HashMap::from([
+        ("local", "LCL"),
+        ("argument", "ARG"),
+        ("this", "THIS"),
+        ("that", "THAT"),
+    ])
+});
+
+static OPERATOR_MAPPINGS: LazyLock<HashMap<&str, &str>> = LazyLock::new(|| {
+    HashMap::from([
+        ("add", "+"),
+        ("sub", "-"),
+        ("and", "&"),
+        ("or", "|"),
+        ("neg", "-"),
+        ("not", "!"),
+        ("eq", "JEQ"),
+        ("lt", "JLT"),
+        ("gt", "JGT"),
+    ])
+});
+
+pub struct CodeWriter<W: Write> {
+    output: W,
+    program_name: String,
+    num_labels: u32,
+}
+
+impl<W: Write> CodeWriter<W> {
+    pub fn new(output: W, program_name: impl Into<String>) -> Self
+    where
+        W: Write,
+    {
+        CodeWriter {
+            output,
+            program_name: program_name.into(),
+            num_labels: 0,
+        }
+    }
+
+    pub fn write_push(&mut self, segment: &str, index: &str) -> Result<(), Box<dyn Error>> {
+        match segment {
+            "constant" => {
+                writeln!(self.output, "@{index}")?;
+                writeln!(self.output, "D=A")?;
+            }
+            "static" => {
+                writeln!(self.output, "@{}.{index}", self.program_name)?;
+                writeln!(self.output, "D=M")?;
+            }
+            "temp" | "pointer" => {
+                let base_address = if segment == "pointer" { "3" } else { "5" };
+                writeln!(self.output, "@{base_address}")?;
+                writeln!(self.output, "D=A")?;
+                writeln!(self.output, "@{index}")?;
+                writeln!(self.output, "A=D+A")?;
+                writeln!(self.output, "D=M")?;
+            }
+            "local" | "argument" | "this" | "that" => {
+                let segment_var = SEGMENT_MAPPINGS.get(segment).copied().unwrap();
+                writeln!(self.output, "@{segment_var}")?;
+                writeln!(self.output, "D=M")?;
+                writeln!(self.output, "@{index}")?;
+                writeln!(self.output, "A=D+A")?;
+                writeln!(self.output, "D=M")?;
+            }
+            _ => return Err(format!("Error writing push, unknown segment: {}", segment).into()),
+        }
+
+        self.write_increment_sp()?;
+        Ok(())
+    }
+
+    pub fn write_pop(&mut self, segment: &str, index: &str) -> Result<(), Box<dyn Error>> {
+        match segment {
+            "static" => {
+                writeln!(self.output, "@SP")?;
+                writeln!(self.output, "AM=M-1")?;
+                writeln!(self.output, "D=M")?;
+                writeln!(self.output, "@{}.{index}", self.program_name)?;
+                writeln!(self.output, "M=D")?;
+            }
+            "pointer" | "temp" => {
+                let base_address = if segment == "pointer" { "3" } else { "5" };
+                writeln!(self.output, "@{base_address}")?;
+                writeln!(self.output, "D=A")?;
+                writeln!(self.output, "@{index}")?;
+                writeln!(self.output, "D=D+A")?;
+                writeln!(self.output, "@R15")?;
+                writeln!(self.output, "M=D")?;
+                writeln!(self.output, "@SP")?;
+                writeln!(self.output, "AM=M-1")?;
+                writeln!(self.output, "D=M")?;
+                writeln!(self.output, "@R15")?;
+                writeln!(self.output, "A=M")?;
+                writeln!(self.output, "M=D")?;
+            }
+            "local" | "argument" | "this" | "that" => {
+                let segment_var = SEGMENT_MAPPINGS.get(segment).copied().unwrap();
+                writeln!(self.output, "@{segment_var}")?;
+                writeln!(self.output, "D=M")?;
+                writeln!(self.output, "@{index}")?;
+                writeln!(self.output, "D=D+A")?;
+                writeln!(self.output, "@R15")?;
+                writeln!(self.output, "M=D")?;
+                writeln!(self.output, "@SP")?;
+                writeln!(self.output, "AM=M-1")?;
+                writeln!(self.output, "D=M")?;
+                writeln!(self.output, "@R15")?;
+                writeln!(self.output, "A=M")?;
+                writeln!(self.output, "M=D")?;
+            }
+            _ => return Err(format!("Error writing pop, unknown segment: {}", segment).into()),
+        }
+        Ok(())
+    }
+
+    pub fn write_arithmetic(&mut self, command: &str) -> Result<(), Box<dyn Error>> {
+        match command {
+            "add" | "sub" | "and" | "or" => {
+                let op = OPERATOR_MAPPINGS.get(command).copied().unwrap();
+                writeln!(self.output, "@SP")?;
+                writeln!(self.output, "AM=M-1")?;
+                writeln!(self.output, "D=M")?;
+                writeln!(self.output, "A=A-1")?;
+                writeln!(self.output, "M=M{op}D")?;
+            }
+            "neg" | "not" => {
+                let op = OPERATOR_MAPPINGS.get(command).copied().unwrap();
+                writeln!(self.output, "@SP")?;
+                writeln!(self.output, "A=M-1")?;
+                writeln!(self.output, "M={op}M")?;
+            }
+            "eq" | "gt" | "lt" => {
+                self.num_labels += 1;
+                let jmp = OPERATOR_MAPPINGS.get(command).copied().unwrap();
+                let label = format!(
+                    "{}.{}.{}",
+                    self.program_name,
+                    command.to_uppercase(),
+                    self.num_labels
+                );
+                let end_label = format!(
+                    "{}.{}_END.{}",
+                    self.program_name,
+                    command.to_uppercase(),
+                    self.num_labels
+                );
+
+                writeln!(self.output, "@SP")?;
+                writeln!(self.output, "AM=M-1")?;
+                writeln!(self.output, "D=M")?;
+                writeln!(self.output, "A=A-1")?;
+                writeln!(self.output, "D=M-D")?;
+                writeln!(self.output, "@{label}")?;
+                writeln!(self.output, "D;{jmp}")?;
+                writeln!(self.output, "@SP")?;
+                writeln!(self.output, "A=M-1")?;
+                writeln!(self.output, "M=0")?;
+                writeln!(self.output, "@{end_label}")?;
+                writeln!(self.output, "0;JEQ")?;
+                writeln!(self.output, "({label})")?;
+                writeln!(self.output, "@SP")?;
+                writeln!(self.output, "A=M-1")?;
+                writeln!(self.output, "M=-1")?;
+                writeln!(self.output, "({end_label})")?;
+            }
+            _ => {
+                return Err(
+                    format!("Error writing arithmetic, unknown command: {}", command).into(),
+                );
+            }
+        }
+
+        Ok(())
+    }
+
+    // Writes D to the address stored at @SP and increments @SP
+    fn write_increment_sp(&mut self) -> Result<(), Box<dyn Error>> {
+        writeln!(self.output, "@SP")?;
+        writeln!(self.output, "A=M")?;
+        writeln!(self.output, "M=D")?;
+        writeln!(self.output, "@SP")?;
+        writeln!(self.output, "M=M+1")?;
+        Ok(())
+    }
+}
